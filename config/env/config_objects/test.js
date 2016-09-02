2f3a4f76c95f5c0862022025e0782b7dc8dec762
Justin M. Hill <justinmhill@gmail.com>
2016-09-02 01:23:41 -0400

    add configuration testing

---
+++ config/env/config_objects/test.js
@@ -0,0 +1,4 @@
+module.exports = {
+  PORT: 8081,
+  db: 'mongodb://localhost/justinmhill-test'
+}
--- tests/setup.spec.js
+++ tests/setup.spec.js
@@ -4,3 +4,14 @@ tape('Tape should run *.spec.js files', function(t) {
   t.pass("It worked!");
   t.end();
 });
+
+// Server set-up test
+
+tape('Server should load with correct configuration', function(t) {
+  const config = require('../config');
+  const testConfig = require('../config/env/config_objects/test.js');
+
+  t.deepEqual(config.environment, testConfig);
+  t.end();
+});
+
