22a6da305f051b13d025c5afe031377d979dcd29
Justin M. Hill <justinmhill@gmail.com>
2016-09-02 01:08:58 -0400

    refactor config file grabbing function

--- config/env/environment.js
+++ config/env/environment.js
@@ -1,13 +1,17 @@
 const env = process.env.NODE_ENV;
 
+function getConfigFilePath(env) {
+  return `./config_objects/${env}.js`;
+}
+
 // The env module requires an individual config object
 // for each environment (e.g., "production.js") - 
 // the application will crash if unable to retrieve the
 // correct configuration file.
 exports.getConfigurationObject = function() {
   if (env) {
-    const configFilePath = "./config_objects/" + env + ".js";
-    return require(configFilePath);
+    const config = getConfigFilePath(env);
+    return require(config);
   } else {
     throw new Error("NODE_ENV has not been set.");
   }
