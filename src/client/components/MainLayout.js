42e776c6d7b37f69dc755fbebc3248009e52bb46
Justin M. Hill <justinmhill@gmail.com>
2016-10-05 20:16:21 -0400

    add basic react router site layout poc

--- src/client/components/MainLayout.js
+++ src/client/components/MainLayout.js
@@ -1,10 +1,16 @@
 import React from 'react';
-import Test from './Test';
+import { Route, Link } from 'react-router';
+import NavBar from './NavBar.js';
 
 export default class MainLayout extends React.Component {
   render() {
     return (
-      <Test />
+      <div className="pure-g">
+        <NavBar />
+        <div className="content pure-u-1 pure-u-md-3-4 ">
+          {this.props.children}
+        </div>
+      </div>
     );
   }
 }
--- src/client/main.js
+++ src/client/main.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, browserHistory } from 'react-router';
-import MainLayout from './components/MainLayout';
+import MainLayout from './components/MainLayout.js';
+import Home from './components/Home.js';
+import About from './components/About.js';
+import Blog from './components/Blog.js';
+import Contact from './components/Contact.js';
+import Projects from './components/Projects.js';
 
 ReactDOM.render((
   <Router history={browserHistory}>
-    <Route path="/" component={MainLayout}>
+    <Route component={MainLayout}>
+      <Route path="/" component={Home} />
+      <Route path="about" component={About} />
+      <Route path="blog" component={Blog} />
+      <Route path="contact" component={Contact} />
+      <Route path="projects" component={Projects} />
     </Route>
   </Router>
 ), document.getElementById("root"));
