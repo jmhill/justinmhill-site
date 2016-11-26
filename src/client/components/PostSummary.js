976d6e65756271f16fe2d699c47187ca2f1207b0
Justin M. Hill <justinmhill@gmail.com>
2016-11-26 12:09:47 -0500

    add PostSummary element
    closes #21

---
+++ src/client/components/PostSummary.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export default class PostSummary extends React.Component {
+  render() {
+    var props = this.props;
+    
+    return (
+      <section className="post">
+        <h2 className="post-title">
+          <a href="{props.url}">{props.title}</a>
+        </h2>
+        <p className="post-meta">{props.date}</p>
+        <div className="post-description">
+          {props.summary}
+          <a href="{props.url}"> Read more...</a>
+        </div>
+      </section>
+    );
+  }
+}
--- src/client/pages/Home.js
+++ src/client/pages/Home.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import ContentBlock from '../components/ContentBlock.js';
+import PostSummary from '../components/PostSummary.js';
 
 export default class Home extends React.Component {
   render() {
     return (
-      <ContentBlock
-        headerText="Testing"
-        bodyText="This is just a test"
-      />
+      <div>
+        <ContentBlock
+          headerText="Testing"
+          bodyText="This is just a test"
+        />
+        <h2 className="content-subhead">Recent Posts</h2>
+        <PostSummary
+          title="Test post"
+          url="http://google.com"
+          date="11/26/2016"
+          summary="This is just a test post for the Summary component"
+        />
+      </div>
     );
   }
 }
