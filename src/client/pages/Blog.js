a725cb87785ed8626958709a32fb815c085d33ba
Justin M. Hill <justinmhill@gmail.com>
2016-12-14 01:22:27 -0500

    make Blog page component fetch posts from api

--- src/client/pages/Blog.js
+++ src/client/pages/Blog.js
@@ -1,38 +1,33 @@
 import React from 'react';
-import PostSummary from '../components/PostSummary.js'
+import PostSummary from '../components/PostSummary.js';
+import axios from 'axios';
+
 
 export default class Blog extends React.Component {
-  constructor() {
-    super();
-    this.posts = [
-      {
-        title: "Title 1",
-        url: "http://google.com",
-        date: "12/1/2016",
-        summary: "Summary of blog post number 1",
-        id: 1
-      },
-      {
-        title: "Title 2",
-        url: "http://google.com",
-        date: "12/2/2016",
-        summary: "Summary of blog post number 2",
-        id: 2
-      },
-      {
-        title: "Title 3",
-        url: "http://google.com",
-        date: "12/3/2016",
-        summary: "Summary of blog post number 3",
-        id: 3
-      }
-    ]
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: []
+    };
+  }
+
+  componentDidMount() {
+    axios.get('/posts')
+      .then( response => {
+        this.setState({
+          posts: response.data.posts
+        });
+      })
+      .catch( error => {
+        console.log(error);
+      });
   }
   
   render() {
+    let posts = this.state.posts;
     return (
       <div>
-        {this.posts.map( post => {
+        {posts.map( post => {
           return (
             <PostSummary 
               title={post.title}
