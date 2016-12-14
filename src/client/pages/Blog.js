04a20e5744515c30604146c89a45bc30d0c102c1
Justin M. Hill <justinmhill@gmail.com>
2016-12-13 23:36:30 -0500

    Add blog page post list and test data

--- src/client/pages/Blog.js
+++ src/client/pages/Blog.js
@@ -1,9 +1,49 @@
 import React from 'react';
+import PostSummary from '../components/PostSummary.js'
 
 export default class Blog extends React.Component {
+  constructor() {
+    super();
+    this.posts = [
+      {
+        title: "Title 1",
+        url: "http://google.com",
+        date: "12/1/2016",
+        summary: "Summary of blog post number 1",
+        id: 1
+      },
+      {
+        title: "Title 2",
+        url: "http://google.com",
+        date: "12/2/2016",
+        summary: "Summary of blog post number 2",
+        id: 2
+      },
+      {
+        title: "Title 3",
+        url: "http://google.com",
+        date: "12/3/2016",
+        summary: "Summary of blog post number 3",
+        id: 3
+      }
+    ]
+  }
+  
   render() {
     return (
-      <p>Hello Blog page</p>
+      <div>
+        {this.posts.map( post => {
+          return (
+            <PostSummary 
+              title={post.title}
+              url={post.url}
+              date={post.date}
+              summary={post.summary}
+              key={post.id}
+            />
+          );
+        })}
+      </div>
     );
   }
 }
