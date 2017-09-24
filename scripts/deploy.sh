6b05ffb0e2b80dd3d1639623a11b3593a1ee9da0
Justin Hill <justinmhill@gmail.com>
2017-09-24 18:03:59 +0000

    Update deploy.sh for testing

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
-cd /var/www/justinmhill-site
-git clean -fd
-npm install
-npm run build
-chmod +x server.js
-sudo systemctl restart justinmhill-site
+# Reference only - old deploy script from snap-ci
+# cd /var/www/justinmhill-site
+# git clean -fd
+# npm install
+# npm run build
+# chmod +x server.js
+# sudo systemctl restart justinmhill-site
+
+echo "Deploy script running..."
